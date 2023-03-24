2650fb8215dbeeb712786d939203f47ba0b7b036
Darko Karapandza <bVNyDUKE@protonmail.com>
2023-03-24 09:39:12 +0100

    revert blame line display

--- after/plugin/gitsigns.lua
+++ after/plugin/gitsigns.lua
@@ -4,7 +4,7 @@ require('gitsigns').setup {
   current_line_blame = true,
   current_line_blame_opts = {
     virt_text = true,
-    virt_text_pos = 'overlay', -- 'eol' | 'overlay' | 'right_align'
+    virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
     delay = 1000,
     ignore_whitespace = false,
   },
