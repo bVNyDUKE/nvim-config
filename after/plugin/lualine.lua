eb42d7c8a1b3367c4dc56f7b1393e20dcfd26591
Darko Ivkov <bVNyDUKE@protonmail.com>
2023-04-03 00:42:09 +0200

    use kanagawa theme

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -1,7 +1,7 @@
 require('lualine').setup {
   options = {
     icons_enabled = true,
-    theme = 'auto',
+    theme = 'moonfly',
     component_separators = { left = '', right = '' },
     section_separators = { left = '', right = '' },
     disabled_filetypes = {'NvimTree'},
--- lua/kpanda/init.lua
+++ lua/kpanda/init.lua
@@ -7,4 +7,47 @@ require('onedark').setup {
   style = 'dark'
 }
 
-vim.cmd("colorscheme catppuccin-macchiato")
+require('kanagawa').setup({
+  keywordStyle = {bold = true},
+  undercurl = true,
+  colors = {
+    theme = {
+      all = {
+        ui = {
+          bg_gutter = "none"
+        }
+      }
+    }
+  },
+  overrides = function(colors)
+    local theme = colors.theme
+    return {
+        NormalFloat = { bg = "none" },
+        FloatBorder = { bg = "none" },
+
+        -- Save an hlgroup with dark background and dimmed foreground
+        -- so that you can use it where your still want darker windows.
+        -- E.g.: autocmd TermOpen * setlocal winhighlight=Normal:NormalDark
+        NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
+
+        -- Popular plugins that open floats will link to NormalFloat by default;
+        -- set their background accordingly if you wish to keep them dark and borderless
+        -- LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+        MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+        TelescopeTitle = { fg = theme.ui.special, bold = true },
+        TelescopePromptNormal = { bg = theme.ui.bg_p1 },
+        TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
+        TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },
+        TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
+        TelescopePreviewNormal = { bg = theme.ui.bg_dim },
+        TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
+        Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 },
+        PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
+        PmenuSbar = { bg = theme.ui.bg_m1 },
+        PmenuThumb = { bg = theme.ui.bg_p2 },
+    }
+  end,
+})
+
+-- vim.cmd("colorscheme catppuccin-macchiato")
+vim.cmd("colorscheme kanagawa-wave")
