7db1f98d1033ce6ebf665684a006f6ed5a25315d
Darko Ivkov <bVNyDUKE@protonmail.com>
2023-04-02 16:24:30 +0200

    nvim tree fixes

--- after/plugin/nvimtree.lua
+++ after/plugin/nvimtree.lua
@@ -1,13 +1,17 @@
 require("nvim-tree").setup({
   hijack_netrw = false,
   git = {
-    enable = false
+    enable = true
   },
   view = {
     side = "right",
     number = true,
     relativenumber = true,
     hide_root_folder = true,
+    width = {
+      min = 50,
+      max = 150
+    }
   },
   diagnostics = {
     enable = true
