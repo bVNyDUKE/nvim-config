cad311ad14446efb3cd95c83b03ff6ec0bd6eb08
Darko Ivkov <bVNyDUKE@protonmail.com>
2024-03-03 16:02:33 +0100

    adjust theme

--- lua/kpanda/init.lua
+++ lua/kpanda/init.lua
@@ -7,18 +7,28 @@ require("kpanda.telescope")
 --vim.cmd("colorscheme github_dark_dimmed")
 
 -- require("gruvbox").setup({
---   contrast = "hard",
---   inverse = true,
---   overrides = {
---     LineNr = { fg = "grey" },
---   },
---   palette_overrides={
---     bright_red = "#de5050"
---   }
+-- 	contrast = "hard",
+-- 	inverse = true,
+-- 	overrides = {
+-- 		LineNr = { fg = "grey" },
+-- 	},
+-- 	palette_overrides = {
+-- 		bright_red = "#de5050",
+-- 	},
 -- })
 -- vim.o.background = "dark"
 -- vim.cmd("colorscheme gruvbox")
 
+require("kanagawa").setup({
+	theme = "wave",
+	overrides = function()
+		return {
+			NormalFloat = { bg = "none" },
+			FloatBorder = { bg = "none" },
+			FloatTitle = { bg = "none" },
+		}
+	end,
+})
 vim.cmd("colorscheme kanagawa")
 
 vim.api.nvim_create_autocmd("TextYankPost", {
