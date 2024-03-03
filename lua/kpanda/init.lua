ff62a692bd394379a8f48177110584ecb22069e3
Darko Ivkov <bVNyDUKE@protonmail.com>
2024-03-03 12:37:09 +0100

    highlight  yank

--- lua/kpanda/init.lua
+++ lua/kpanda/init.lua
@@ -20,3 +20,11 @@ require('kpanda.telescope')
 -- vim.cmd("colorscheme gruvbox")
 
 vim.cmd("colorscheme kanagawa")
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking',
+  group = vim.api.nvim_create_augroup('k-highlight-yank', {clear = true}),
+  callback = function ()
+    vim.highlight.on_yank()
+  end
+})
