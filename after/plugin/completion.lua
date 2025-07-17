f6fc1a2de108611c6e7a6da0b833d4a06130d671
Darko Ivkov <bVNyDUKE@protonmail.com>
2025-07-17 22:07:26 +0200

    no fuzzy completion

--- after/plugin/completion.lua
+++ after/plugin/completion.lua
@@ -2,6 +2,6 @@ require("mini.completion").setup({
 	lsp_completion = { source_func = "omnifunc", auto_setup = false },
 })
 
-vim.opt.completeopt = { "noinsert", "menuone", "fuzzy", "noselect" }
+vim.opt.completeopt = { "noinsert", "menuone", "noselect" }
 vim.cmd([[au FileType snacks_picker_input lua vim.b.minicompletion_disable = true]])
 vim.cmd([[au FileType snacks_input lua vim.b.minicompletion_disable = true]])
