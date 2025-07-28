6bb174f4cb601c54c6db546a99372cf41cf1f71d
Darko Ivkov <bVNyDUKE@protonmail.com>
2025-07-28 21:12:27 +0200

    simpler lualine

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -1,13 +1,9 @@
 require("lualine").setup({
 	options = {
-		icons_enabled = true,
-		-- theme = 'gruvbox_dark',
+		icons_enabled = false,
 		theme = "auto",
-		-- component_separators = { left = '', right = '' },
-		-- section_separators = { left = '', right = '' },
-		component_separators = { left = "|", right = "|" },
-		section_separators = { left = "", right = "" },
-		disabled_filetypes = { "NvimTree" },
+		component_separators = "",
+		section_separators = "",
 		always_divide_middle = true,
 		globalstatus = false,
 	},
