47676e89311b021ef97f35a27cded868bc63161e
Darko Ivkov <bVNyDUKE@protonmail.com>
2025-07-30 21:40:12 +0200

    clean up init

--- lua/kpanda/init.lua
+++ lua/kpanda/init.lua
@@ -2,22 +2,6 @@ vim.loader.enable()
 require("kpanda.set")
 require("kpanda.vimplug")
 
---vim.cmd("colorscheme monokai-pro")
---vim.cmd("colorscheme github_dark_dimmed")
-
--- require("gruvbox").setup({
--- 	contrast = "hard",
--- 	inverse = true,
--- 	overrides = {
--- 		LineNr = { fg = "grey" },
--- 	},
--- 	palette_overrides = {
--- 		bright_red = "#de5050",
--- 	},
--- })
--- vim.o.background = "dark"
--- vim.cmd("colorscheme gruvbox")
-
 require("kanagawa").setup({
 	theme = "wave",
 	overrides = function()
@@ -29,11 +13,3 @@ require("kanagawa").setup({
 	end,
 })
 vim.cmd("colorscheme kanagawa")
-
-vim.api.nvim_create_autocmd("TextYankPost", {
-	desc = "Highlight when yanking",
-	group = vim.api.nvim_create_augroup("k-highlight-yank", { clear = true }),
-	callback = function()
-		vim.highlight.on_yank()
-	end,
-})
