7a97a94db3f4a1a8a8c0782094f4a7ace11bfffa
Darko Ivkov <bVNyDUKE@protonmail.com>
2025-08-07 21:43:25 +0200

    update statusline

--- after/plugin/statusline.lua
+++ after/plugin/statusline.lua
@@ -8,28 +8,58 @@ local cmp = {} -- statusline components
 -- Example pattern: %#SomeHighlight#some-text%*
 local hi_pattern = "%%#%s#%s%%*"
 
+local function orange(text)
+	return hi_pattern:format("DiagnosticWarn", text)
+end
+
+local function red(text)
+	return hi_pattern:format("DiagnosticWarn", text)
+end
+
+local function green(text)
+	return hi_pattern:format("DiagnosticOk", text)
+end
+
+local function hint(text)
+	return hi_pattern:format("DiagnosticHint", text)
+end
+
 function _G._statusline_component(name)
 	return cmp[name]()
 end
 
-local modes = {
-	["n"] = "NORMAL",
-	["nt"] = "TERM-NORM",
-	["V"] = "VISUAL",
-	["v"] = "VISUAL",
-	["i"] = "INSERT",
-	["c"] = "COMMAND",
-	["t"] = "TERMINAL",
-}
+function cmp.git_status()
+	local git_info = vim.b.gitsigns_status_dict
+	if not git_info or git_info.head == "" then
+		return ""
+	end
 
-function cmp.mode_name()
-	local mode = vim.api.nvim_get_mode().mode
-	local mode_name = modes[mode]
-	return hi_pattern:format("Search", " " .. (mode_name and mode_name or mode) .. " ")
+	local head = git_info.head
+	local added = git_info.added and (" +" .. git_info.added) or ""
+	local changed = git_info.changed and (" ~" .. git_info.changed) or ""
+	local removed = git_info.removed and (" -" .. git_info.removed) or ""
+	if git_info.added == 0 then
+		added = ""
+	end
+	if git_info.changed == 0 then
+		changed = ""
+	end
+	if git_info.removed == 0 then
+		removed = ""
+	end
+
+	return table.concat({
+		"[ ", -- branch icon
+		head,
+		added,
+		changed,
+		removed,
+		"]",
+	})
 end
 
 function cmp.diagnostic_status()
-	local ok = " OK "
+	local ok = "[OK]"
 
 	local ignore = {
 		["c"] = true, -- command mode
@@ -42,33 +72,37 @@ function cmp.diagnostic_status()
 		return ok
 	end
 
-	local levels = vim.diagnostic.severity
-	local errors = #vim.diagnostic.get(0, { severity = levels.ERROR })
-	local warnings = #vim.diagnostic.get(0, { severity = levels.WARN })
-	local show_diagnostics = errors > 0 or warnings > 0
+	--- @param levels {color: function, symbol: string, level: vim.diagnostic.Severity}[]
+	--- @return string[]
+	local getLevels = function(levels)
+		local res = {}
+		for _, value in ipairs(levels) do
+			local count = #vim.diagnostic.get(0, { severity = value.level })
+			if count > 0 then
+				table.insert(res, value.color(table.concat({ " ", value.symbol, ":", count, " " })))
+			end
+		end
+		return res
+	end
 
-	if not show_diagnostics then
+	local levels = vim.diagnostic.severity
+	local level_counts = getLevels({
+		{ level = levels.ERROR, color = red, symbol = "E" },
+		{ level = levels.WARN, color = orange, symbol = "W" },
+		{ level = levels.INFO, color = green, symbol = "I" },
+		{ level = levels.HINT, color = hint, symbol = "H" },
+	})
+	if #level_counts == 0 then
 		return ok
 	end
 
-	local error_count = ""
-	local warn_count = ""
-
-	if errors > 0 then
-		error_count = hi_pattern:format("DiagnosticSignError", " E:" .. errors .. " ")
-	end
-
-	if warnings > 0 then
-		warn_count = hi_pattern:format("DiagnosticSignWarn", " W:" .. warnings .. " ")
-	end
-
-	return error_count .. warn_count
+	return table.concat({ "[", table.concat(level_counts), "]" })
 end
 
 local statusline = {
-	"%{FugitiveStatusline()} ",
 	'%{%v:lua._statusline_component("diagnostic_status")%} ',
-	"%t ",
+	'%{%v:lua._statusline_component("git_status")%} ',
+	"%f ",
 	"%r",
 	"%m",
 	"%=",
