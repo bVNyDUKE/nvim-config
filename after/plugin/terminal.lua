2460dd35774e4edb1845c6fd3b26d876cb043cec
Darko Ivkov <bVNyDUKE@protonmail.com>
2025-08-13 11:05:03 +0200

    local term settings

--- after/plugin/terminal.lua
+++ after/plugin/terminal.lua
@@ -1,8 +1,8 @@
 vim.api.nvim_create_autocmd("TermOpen", {
 	group = vim.api.nvim_create_augroup("custom-termopen", { clear = true }),
 	callback = function()
-		vim.opt.number = false
-		vim.opt.relativenumber = false
+		vim.opt_local.number = false
+		vim.opt_local.relativenumber = false
 	end,
 })
 
