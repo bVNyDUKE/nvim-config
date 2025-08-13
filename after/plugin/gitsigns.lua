a0157b1018be6a43c8fccb81e3d27dc80fe2fc28
Darko Ivkov <bVNyDUKE@protonmail.com>
2025-08-13 12:20:10 +0200

    update gitsigns

--- after/plugin/gitsigns.lua
+++ after/plugin/gitsigns.lua
@@ -1,34 +1,56 @@
 local gs = require("gitsigns")
-
 gs.setup({
 	sign_priority = 3,
 	on_attach = function(bufnr)
 		local function map(mode, lhs, rhs, opts)
-			opts = vim.tbl_extend("force", { noremap = true, silent = true }, opts or {})
-			vim.api.nvim_buf_set_keymap(bufnr, mode, lhs, rhs, opts)
+			opts = opts or {}
+			opts.buffer = bufnr
+			vim.keymap.set(mode, lhs, rhs, opts)
 		end
 
-		map("n", "]c", "&diff ? ']c' : '<cmd>Gitsigns next_hunk<CR>'", { expr = true })
-		map("n", "[c", "&diff ? '[c' : '<cmd>Gitsigns prev_hunk<CR>'", { expr = true })
+		map("n", "]c", function()
+			if vim.wo.diff then
+				vim.cmd.normal({ "]c", bang = true })
+			else
+				---@diagnostic disable-next-line
+				gs.nav_hunk("next", { wrap = true })
+			end
+		end)
 
-		map("n", "ghs", ":Gitsigns stage_hunk<CR>")
-		map("v", "ghs", ":Gitsigns stage_hunk<CR>")
-		map("n", "ghu", ":Gitsigns reset_hunk<CR>")
-		map("v", "ghu", ":Gitsigns reset_hunk<CR>")
-		map("n", "ghS", "<cmd>Gitsigns stage_buffer<CR>")
-		map("n", "ghU", "<cmd>Gitsigns undo_stage_hunk<CR>")
-		map("n", "ghR", "<cmd>Gitsigns reset_buffer<CR>")
-		map("n", "ghp", "<cmd>Gitsigns preview_hunk<CR>")
-		map("n", "gbb", ":Gitsigns blame<CR>")
-		vim.keymap.set("n", "ghb", function()
+		map("n", "[c", function()
+			if vim.wo.diff then
+				vim.cmd.normal({ "[c", bang = true })
+			else
+				---@diagnostic disable-next-line
+				gs.nav_hunk("prev")
+			end
+		end)
+
+		map("n", "ghs", gs.stage_hunk)
+		map("n", "ghu", gs.reset_hunk)
+
+		map("v", "<leader>hs", function()
+			gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+		end)
+
+		map("v", "<leader>hr", function()
+			gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+		end)
+
+		map("n", "ghS", gs.stage_buffer)
+		map("n", "ghR", gs.reset_buffer)
+
+		map("n", "ghp", gs.preview_hunk)
+		map("n", "gbb", gs.blame)
+		map("n", "ghb", function()
 			gs.blame_line({ full = true })
 		end)
-		map("n", "gtb", "<cmd>Gitsigns toggle_current_line_blame<CR>")
-		map("n", "ghd", "<cmd>Gitsigns diffthis<CR>")
-		map("n", "ghD", '<cmd>lua require"gitsigns".diffthis("~")<CR>')
-		map("n", "gtd", "<cmd>Gitsigns toggle_deleted<CR>")
-
-		map("o", "ih", ":<C-U>Gitsigns select_hunk<CR>")
-		map("x", "ih", ":<C-U>Gitsigns select_hunk<CR>")
+		map("n", "gtb", gs.toggle_current_line_blame)
+		map("n", "ghd", gs.diffthis)
+		map("n", "ghD", function()
+			---@diagnostic disable-next-line
+			gs.diffthis("~")
+		end)
+		map({ "o", "x" }, "ih", gs.select_hunk)
 	end,
 })
